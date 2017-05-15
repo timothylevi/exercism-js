3c7af842ea77c54c977a9f81ab0be701f18f7f92
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-15 13:42:40 -0400

    Compute hamming differences between strings of equal length

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -3,7 +3,18 @@ var Hamming = function() {
 };
 
 Hamming.prototype.compute = function compute(strand1, strand2) {
-  if (strand1 === strand2) return 0;
+  function getDifferences(char, index) {
+    return char === strand2[index] ? 0 : 1;
+  }
+
+  function sumDifferences(sum, difference) {
+    return sum + difference;
+  }
+
+  return strand1
+    .split('')
+    .map(getDifferences)
+    .reduce(sumDifferences);
 };
 
 module.exports = Hamming;
