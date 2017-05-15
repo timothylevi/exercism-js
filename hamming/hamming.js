5becd1482bc46e66a39af637eeb07a9bf5de58f7
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-15 13:44:35 -0400

    Throws an error when strand lengths are not equal

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -3,6 +3,10 @@ var Hamming = function() {
 };
 
 Hamming.prototype.compute = function compute(strand1, strand2) {
+  if (strand1.length !== strand2.length) {
+    throw new Error('DNA strands must be of equal length.');
+  }
+
   function getDifferences(char, index) {
     return char === strand2[index] ? 0 : 1;
   }
