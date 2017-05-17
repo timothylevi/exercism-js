13bfaa4557f24e1e79a9af2d0500d1db08dcb310
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-16 22:22:30 -0400

    Replace forEach with reducer and consolidate return statement

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,32 +1,20 @@
 var Pangram = function(sentence) {
+  this.ALPHABET_LENGTH = 26;
   this.value = sentence;
 };
 
 Pangram.prototype.isPangram = function isPangram() {
-  // ES2015
   if (!this.value) return false;
 
-  var alphabet_length = 26;
-  var sentence_set = {};
-
-  this.value
+  var sentence = this.value
     .replace(/[\d\W_]+/g)
     .toLowerCase()
     .split('')
-    .forEach(function(value) {
-      sentence_set[value] = true;
-    });
+    .reduce(function(sentence_set, letter) {
+      sentence_set[letter] = true;
+    }, {});
 
-  if (Object.keys(sentence_set).length === alphabet_length) return true;
-
-  return false;
-
-  // ES6
-  // var sentence = this.value.replace(/[\d\W_]+/g).toLowerCase();
-  // let sentence_set = new Set(sentence);
-  // let alphabet_len = 26;
-  //
-  // return sentence_set.size === alphabet_len;
+  return Object.keys(sentence).length === this.ALPHABET_LENGTH;
 };
 
 module.exports = Pangram;
